f44a7bc1e97dcc5fc7e396e10f9d562563c87930
satvk <satvikkatoch@gmail.com>
2024-09-07 11:06:47 +0530

    changed description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Dania's Farewell",
-  description: "Adios Brather :)",
+  description: "Adios Amigo :)",
 };
 
 export default function RootLayout({
